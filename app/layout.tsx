e384d93d58bf9ee1ad42477b7df7574c60e261f5
Camila Maron <camila.maron93@gmail.com>
2025-07-21 16:07:25 -0300

    favicon was added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import type React from 'react';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import '../styles/globals.css';
+import { Inter } from 'next/font/google';
 import { LanguageProvider } from '@/contexts/language-context';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Camila Maron - Full Stack Developer',
-  description:
-    'Software Developer with 8+ years of experience designing, developing, testing, and maintaining scalable enterprise applications across healthcare, gaming, and business domains. Adept at full stack development using modern technologies such as React, Node.js, AWS, and cloud microservices. Strong collaborator with cross-functional teams, focused on delivering high-quality digital products. Seeking a growth-oriented company where I can contribute with technical excellence and continue advancing my professional development.',
-  generator: 'v0.dev',
-};
 
 export default function RootLayout({
   children,
@@ -20,6 +13,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Camila Maron - Full Stack Developer</title>
+        <meta
+          name="description"
+          content="Software Developer with 8+ years of experience designing, developing, testing, and maintaining scalable enterprise applications across healthcare, gaming, and business domains. Adept at full stack development using modern technologies such as React, Node.js, AWS, and cloud microservices. Strong collaborator with cross-functional teams, focused on delivering high-quality digital products. Seeking a growth-oriented company where I can contribute with technical excellence and continue advancing my professional development."
+        />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className={inter.className}>
         <LanguageProvider>{children}</LanguageProvider>
       </body>
